81a1de5c39ade73102a1cfb01d83b0fb1f5de6f6
Taddes Korris <taddes.korris@gmail.com>
2018-06-15 23:15:41 -0400

    user request path stored as parsed url

---
+++ index.js
@@ -0,0 +1,39 @@
+/* 
+* Primary file for API
+* 
+*/
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// Server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+
+  // Get the URL and parse
+  /* request object contains lots of data, second param true to parse query string, 
+  *  set it as if we sent to query string module. Parsed metadata*/
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get path from URL. Untrimmed path that the user requested.
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  console.log(`path ${path}`);
+ 
+
+  // Send response
+  res.end('Hello World\n')
+
+  // Log path the user requested
+  console.log(`Request recieved on path: ${trimmedPath}`);
+
+
+});
+
+
+// Start server, have it listen on port 3000
+PORT = process.env.PORT || 3000
+server.listen(3000, () => {
+  console.log(`Server listening on ${PORT}`)
+});
